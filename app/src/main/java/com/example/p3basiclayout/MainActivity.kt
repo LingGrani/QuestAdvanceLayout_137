a6ab15643c5f6a98f73cbfd70168abc4bc7fcb3f
LingGrani <luthfiangger2510@gmail.com>
2024-10-21 17:41:08 +0700

    Initial commit

---
+++ app/src/main/java/com/example/p3basiclayout/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.p3basiclayout
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.p3basiclayout.ui.theme.P3BasicLayoutTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            P3BasicLayoutTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    BelajarLayout(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    P3BasicLayoutTheme {
+        Greeting("Android")
+    }
+}
